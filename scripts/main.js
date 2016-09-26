16ce3b6d35a612c34d10ce676d5aab52a07d9f15
Mick Lin <cl3469@columbia.edu>
2016-09-25 23:33:17 -0400

    set text done and btn done

--- scripts/main.js
+++ scripts/main.js
@@ -8,48 +8,42 @@ document.addEventListener('DOMContentLoaded', function() {
     $('#switch-labelText').bootstrapSwitch("offText", 'x');
 
     var button = document.getElementById('download');
+    button.disabled = true;
     var keys = [];
     //var callback_data ;
     var folder;
+    var rdata ; 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {data: null}, function(response) {
-                var rdata = response.data;
+                rdata = response.data;
                 Object.keys(rdata).forEach(function(key) {
                         keys.push(key);
                 });
                 folder = tabs[0].url + "_";
-                console.log(keys);
-                //$('#switch-labelText').bootstrapSwitch("labelText", folder);
-                $('#switch-labelText').bootstrapSwitch("labelText", keys[0]+"("+rdata[keys[0]].length+")");
-                // for(var i in keys){
-                //     var fragment = create('<input id='+keys[i]+'type="checkbox">');
-                //     // var tmpl ='<input id="#{id}" type="checkbox">';
-                //     // var vals = { id : keys[i]};
-                //     // alert($.tmpl(tmpl, vals));
-                //     document.getElementById('form').appendChild(fragment);
-                // }
-                //document.getElementById('switch-labelText').text = keys[0];
-            });
-    });
-    //alert(keys);
-    keys.push(".jpg");
-    var tree = document.createDocumentFragment();
-    for(var i in keys){
+                if(keys.length !=0){button.disabled = false;} 
+                var tree = document.createDocumentFragment();
+                for(var i in keys){
                     var checkbox = document.createElement('input');
                     checkbox.type = "checkbox";
                     checkbox.id = keys[i].substring(1);
                     tree.appendChild(checkbox);
-    }
-    document.getElementById('form').appendChild(tree);
-    for(var i in keys){
+                }
+                document.getElementById('form').appendChild(tree);
+                for(var i in keys){
                     var str = keys[i].substring(1);
                     $("#"+str).bootstrapSwitch();
                     $("#"+str).bootstrapSwitch("state", true);
                     $("#"+str).bootstrapSwitch("size", 'mini');
                     $("#"+str).bootstrapSwitch("onText", 'o');
                     $("#"+str).bootstrapSwitch("offText", 'x');
+                    $("#"+str).bootstrapSwitch("labelText", str+"("+rdata[keys[i]].length+")");
 
-    }
+                }
+            
+            });
+    });
+
+   
     button.addEventListener('click', function () {
         folder += new Date().toString();
         // $('#status').html('Clicked change links button');
